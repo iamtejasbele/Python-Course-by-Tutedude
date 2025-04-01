c3406b67ce826bd903ae184d811543d86c47de7f
iamtejasbele <tejas.bele25@gmail.com>
2025-04-01 12:06:55 +0530

    Update Assign_1_Task_1.py

--- Assign_1_Task_1.py
+++ Assign_1_Task_1.py
@@ -4,4 +4,7 @@ b = int(input('Enter the second number:'))
 print('Addition =', a+b)
 print('Subtraction =', a-b)
 print('Multiplication =', a*b)
-print('Division =', a/b)
+try:
+    print('Division =', a/b)
+except:
+    print('Division = Error: Division by zero is not allowed')
